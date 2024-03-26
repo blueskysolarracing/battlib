4de8125b7be11152cb7fbb821cca7ea4c999ca7e
shay222 <95372373+shay222@users.noreply.github.com>
2024-03-26 16:56:14 -0400

    Implemented RMS calculation

--- battlib/cc.py
+++ battlib/cc.py
@@ -17,8 +17,18 @@ class CCSOCEstimator(SOCEstimator):
     def __init__(self, battery, initial_voltage):
         self.battery = battery
         self.soc = battery.interp_soc(initial_voltage)
+        self.soc_errors = []
 
-    def step(self, *, dt, i_in, measured_v=None):
+    def step(self, *, dt, i_in, measured_v=None, actual_soc=None):
         dq = -i_in * dt
         self.soc += dq / self.battery.q_cap
         self.soc = np.clip(self.soc, 0, 1)
+
+        if actual_soc:
+            soc_error = actual_soc - self.soc
+            self.soc_errors.append(soc_error)
+
+    def rms_calculation(self):
+        soc_errors_squared = np.square(self.soc_errors)
+        rms = np.sqrt(np.mean(soc_errors_squared))
+        return rms
