bfe1bc6bc18bd4243063618dd88aebd979360262
shay222 <95372373+shay222@users.noreply.github.com>
2024-03-26 17:04:42 -0400

    Implemented RMS calculation

--- battlib/ocv.py
+++ battlib/ocv.py
@@ -19,6 +19,16 @@ class OCVSOCEstimator(SOCEstimator):
     def __init__(self, battery, initial_voltage):
         self.battery = battery
         self.soc = battery.interp_soc(initial_voltage)
+        self.soc_errors = []
 
-    def step(self, *, dt=None, i_in=None, measured_v):
+    def step(self, *, dt=None, i_in=None, measured_v, actual_soc=None):
         self.soc = self.battery.interp_soc(measured_v)
+
+        if actual_soc:
+            soc_error = actual_soc - self.soc
+            self.soc_errors.append(soc_error)
+
+    def rms_calculation(self):
+        soc_errors_squared = np.square(self.soc_errors)
+        rms = np.sqrt(np.mean(soc_errors_squared))
+        return rms
